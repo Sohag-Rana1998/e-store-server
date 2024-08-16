b8d0b548aacd48e4d9363417236821a22c163991
sohagislam1998 <sohagislambd1998@gmail.com>
2024-08-16 15:25:59 +0600

    mongodb connected and data  fetched

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const products = client.db("E-Store").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const result = await products.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
